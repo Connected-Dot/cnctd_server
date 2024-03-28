4a18fa62736b0aa8c82da530e9a442e89d9e93a4
kebtech <64436886+kebtech@users.noreply.github.com>
2024-03-28 09:31:30 -0400

    updated router to use copy

--- src/router.rs
+++ src/router.rs
@@ -2,6 +2,6 @@ use std::{future::Future, pin::Pin};
 
 use crate::{message::Message, response::Response};
 
-pub trait RouterFunction: Send + Sync + Clone {
+pub trait RouterFunction: Send + Sync + Clone + Copy {
     fn route(&self, msg: Message) -> Pin<Box<dyn Future<Output = Response> + Send>>;
 }
